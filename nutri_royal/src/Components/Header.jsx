962920107c474b9cfa51c4c40af1a4e15aaaba89
“Rocio <dev.alvarezbernal@gmail.com>
2020-05-16 15:20:16 -0500

    logo added to header

--- nutri_royal/src/Components/Header.jsx
+++ nutri_royal/src/Components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import './App.css';
+import Nutriroyal from './../images/Nutriroyal.png'
 
 function Header() {
   return (
       <nav className="Header">
-          <img id="logo" src="./../images/Nutriroyal.png"/>
+          <img id="logo" src={Nutriroyal} />
           <ul id="NavBarBttns">
             <li><a href="#">My Account</a></li>
             <li><a href="#">Cart</a></li>
