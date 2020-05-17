b22ec1ef0071f6c30205da2bf465ec8b271879f4
KevinJ.Regis <dev.regis1@gmail.com>
2020-05-17 01:24:48 -0500

    added form content

--- nutri_royal/src/App.js
+++ nutri_royal/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import ReactForm from './components';
+import React, { Component }from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="text-box">
-      <textarea placeholder="Comments"> </textarea>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    displayBoxInput: {}
+  };
+  // onsubmit shows user input in concole
+  // console.log(`What the user just entered: ${displayBoxInput}`) --> what it says
+    // this.setState(`${displayBoxInput}`) - and below renders to page
 
-export default App;
+    onSubmit = displayBoxInput => { 
+      this.setState({displayBoxInput})
+     }
+
+  render() { 
+    return ( 
+      <div>
+        {/* //onSubmit=displaBoxIn... shows user input in concole */}
+        {/* 2. inside of ptag will be an empty object and JSON stringify makes it looks "nice" */}
+        <ReactForm onSubmit={displayBoxInput => this.onSubmit(displayBoxInput) }/>
+          <p id="display-input" > {JSON.stringify(this.state.displayBoxInput, null, 2)}</p>
+      </div>
+     );
+  }
+}
+ 
+export default App;     
