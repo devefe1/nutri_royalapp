a4e8e3ab2ee2d90b01e341ef3a26a480c4a36a6a
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-05-19 04:28:42 -0500

    menu

--- nutri_royal/src/Components/Menu.jsx
+++ nutri_royal/src/Components/Menu.jsx
@@ -3,10 +3,24 @@ import React from 'react';
 
 function Menu() {
   return (
-      <div className="Menu">
-          Menu with items 
+      <div className="menuContainer">
+         <nav className="navBar">
+           <div className="burger-menu">
+             <div className="line1"></div>
+             <div className="line2"></div>
+             <div className="line3"></div>
+           </div>
+
+           <ul className= "navList">
+             <li className="nav-item">
+               <a href = "#" className="nav-link">CheeseBurger</a>
+             </li>
+           </ul>
+         </nav>
       </div>
   );
 }
 
 export default Menu;
+
+
